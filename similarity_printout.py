b63075c37e687948551d211f377d5617715ed3dc
sigmaroles <sohhom@gmail.com>
2018-04-09 12:10:19 +0530

    cleaned similarity_printout.py

--- similarity_printout.py
+++ similarity_printout.py
@@ -1,20 +1,4 @@
-
-
-from gensim.models import Word2Vec
 from util_kw import *
-import numpy as np
-import itertools
-import os
-from scipy import spatial
-
-
-path_models = 'word2vec_models'
-journalfilenames = ['IN-thehindu-opinion_with_phraser',
-'IN-indianexpress-india_with_phraser',
- 'IN-indianexpress-opinion_with_phraser',
- 'IN-indianexpress-editorials_with_phraser',
- 'IN-thetimesofindia_00_with_phraser',
- 'IN-thetimesofindia_01_with_phraser']
 
 if __name__=='__main__':
     print ("\n\n **** Upper caste keywords and negative aspect ****")
@@ -27,4 +11,4 @@ if __name__=='__main__':
     print_similarities_between_k_lists(lower_caste_keywords, priviledge_keywords)
 
     print ("\n\n **** Neutral keywords and priviledge keywords ****")
-    print_similarities_between_k_lists(neutral_keywords, )
+    print_similarities_between_k_lists(neutral_keywords, priviledge_keywords)
